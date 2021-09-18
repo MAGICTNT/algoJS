dc450833f22393e04ab7cbcc671cf379fb0e6627
MAGICTNT <78156199+MAGICTNT@users.noreply.github.com>
2021-09-18 16:44:30 +0200

    Update script.js

--- trie_a_bull/script.js
+++ trie_a_bull/script.js
@@ -2,16 +2,19 @@
 
 function bublleSort(array)
 {
-    for (let i = 1; array.length; i++)
+    for (let i = 0; i < array.length; i++)
     {
-        for (let j = 1; array.length; j++)
+        for (let j = 0; j < array.length; j++)
         {
             if (array[j] > array[j + 1]) //? je vairifie que le premier chiffre et superieur au secone et si il l'es
             {
-                [array[j], array[j + 1] = array[j + 1], array[j]] //? j'intervertie le chiffre entre eux
+                // console.log('valeur avant: ' + array[j]);    
+                [array[j], array[j + 1]] = [array[j + 1], array[j]] //? j'intervertie le chiffre entre eux
+                // console.log('valeur apres: ' + array[j]);
             }
         }
     }
+    console.log(array);
     return array;
 }
 console.log(bublleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
