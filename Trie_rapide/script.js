df3018312d59685930220e72f25c06849dba4678
MAGICTNT <78156199+MAGICTNT@users.noreply.github.com>
2021-09-20 17:54:15 +0200

    Update script.js

--- Trie_rapide/script.js
+++ Trie_rapide/script.js
@@ -12,7 +12,7 @@ function quickSort(array)
     }
 
     const pivot = array[array.length - 1];      //todo      on crée une consté pivot (le tableau commence à zéro d'où le -1 pour récupérer le dernier chiffre du tableau)
-    const miniNumber = [];                     //todo      on prépare le tableau de mini pour mettre les petit chiffres 
+    const miniNumber = [];                     //todo      on prépare le tableau de mini pour mettre les petits chiffres 
     const maxiNumber = [];                    //todo      on prépare le tableau de maxi pour mettre les gros chiffres
 
     for (i = 0; i < array.length - 1; i++)
@@ -21,13 +21,13 @@ function quickSort(array)
     }
     if (miniNumber.length > 0 && maxiNumber.length > 0) { //todo    on dit que si jamais il y a quelque chose dans le tableau des mini et maxi alors on lance l'algo
 
-        console.log(miniNumber + " / " + maxiNumber); //? on fait un console.log pour avoir un visuelle de l'opération
+        console.log(miniNumber + " / " + maxiNumber); //? oon fait une console log pour avoir une visuelle de l'opération
 
-          return [...quickSort(miniNumber), pivot, ...quickSort(maxiNumber)]; //? on fait deux straight operator pour que tout ce presente dans un seul tableau
+          return [...quickSort(miniNumber), pivot, ...quickSort(maxiNumber)]; //? on fait deux straight operators pour que tout ce presente dans un seul tableau
     }
         else if (miniNumber.length > 0)             //todo s'il y a quelque chose dans le mini tableau mais pas le maxi on lance cette version 
         {
-            return [...quickSort(miniNumber), pivot]; //! on oublie pas le pivot
+            return [...quickSort(miniNumber), pivot]; //! on n'oublie pas le pivot
         }
         else                                        //todo s'il y a quelque chose dans le maxi tableau mais pas le mini on lance cette version 
         {
